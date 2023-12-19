84e6e306908b3b1040cf4b89ff9e85b88352df2b
zboxjpg <dwplko@gmail.com>
2023-12-19 14:09:11 +0100

    .

--- script.js
+++
@@ -1,8 +0,0 @@
-for(let i=1; i<6; i++){
-    let _ = document.createElement("img");
-    let __ = document.createElement("div");
-    __.className = "grid-element";
-    _.src = "./img/" + i + ".png";
-    document.querySelector(".grid").appendChild(__);
-    __.appendChild(_)
-}
